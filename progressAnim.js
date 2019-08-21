5a38a8b4888e8726c44c8238ce2c6bd982249397
Valentin Misiaszek <mowerv6@gmail.com>
2019-08-21 15:33:02 +0200

    Add progress bar

--- progressAnim.js
+++ progressAnim.js
@@ -1,16 +1,17 @@
 // When doc is ready --> exec anim
-document.addEventListener("DOMContentLoaded", function() {
-  progressBarAnim(75);
+document.addEventListener("DOMContentLoaded", () => {
+  progressBarAnim(75, 20);
+  progressDonutAnim(60, 30);
 });
 
 // Progress bar anim
-function progressBarAnim(percent) {
+function progressBarAnim(percent, interval) {
   let width = 0;
   let bar = document.getElementById("barValue");
   let percentNumber = document.getElementById("barPercent");
 
-  var id = setInterval(frame, 20);
-  function frame() {
+  var id = setInterval(progress, interval);
+  function progress() {
     // If bar progress to his percent goal
     if (width >= percent) {
       // Stop it
@@ -22,4 +23,22 @@ function progressBarAnim(percent) {
       bar.style.width = width + "%";
     }
   }
+}
+
+// Donut progress anim
+function progressDonutAnim(percent, interval) {
+  let circle = document.getElementById("two");
+  let text = document.getElementById("percent-two");
+  let angle = 0;
+  percent = percent * 4.7;
+
+  window.timer = window.setInterval(() => {
+    circle.setAttribute("stroke-dasharray", angle + ", 20000");
+    text.innerHTML = parseInt((angle / 471) * 100) + " %";
+
+    if (angle >= percent) {
+      window.clearInterval(window.timer);
+    }
+    angle += 7;
+  }, interval);
 }
